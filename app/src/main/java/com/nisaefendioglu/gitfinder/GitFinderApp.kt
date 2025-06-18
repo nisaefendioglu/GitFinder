21654578ae3a0851f0c01b8c87884652b1925558
nisaefendioglu <nisaefendioglu0@gmail.com>
2025-06-18 19:45:21 +0300

    Feat: Initialize base project structure and core theme

---
+++ app/src/main/java/com/nisaefendioglu/gitfinder/GitFinderApp.kt
@@ -0,0 +1,7 @@
+package com.nisaefendioglu.gitfinder
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GitFinderApp : Application() {
+}
---
+++ app/src/main/java/com/nisaefendioglu/gitfinder/presentation/ui/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.nisaefendioglu.gitfinder.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.nisaefendioglu.gitfinder.R
+import com.nisaefendioglu.gitfinder.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
